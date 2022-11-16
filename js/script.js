d122ad61862729bfbb1a45fd4fb1ba0f3a1a22f6
ceda18 <cedavelickovic123@gmail.com>
2022-11-16 13:06:10 +0100

    16nov

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 class CustomZaglavlje extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-       
+
         <header>
             <a href="./index.html"><img src="img/header/ABBA_logo_net.png" alt="ABBA logo"></a>
 
@@ -13,9 +13,41 @@ class CustomZaglavlje extends HTMLElement {
                     <li><a href="tour.html">TURNEJA</a></li>
                 </ul>
             </div>
-            <button id="infoButton">Info</button>
+            <button id="infoButton" type="button" onclick="openPopupInfo()">Info</button>
         </header>
 
+        <div id="contact-info" class="contact-info">
+
+        <div id="contact-info-wrapper">
+
+            <img src="/img/contact/info.png" alt="info logo circular transparent">
+            <p>Autori</p>
+            
+            <div class="contact">
+                <a href="https://www.linkedin.com/in/ceda/" target="_blank">
+                    <img src="img/contact/ceda.png" alt="Čeda">
+                </a>
+                <h2>Čeda Veličković<br>2020/0204</h2>
+                
+            </div>
+            <div class="contact">
+                <a href="https://www.linkedin.com/in/%C4%91or%C4%91e-%C4%91or%C4%91evi%C4%87-99a8ab252/" target="_blank">
+                    <img src="img/contact/djole.png" alt="Đole"></a>
+                <h2>Đorđe Đorđević<br>2020/0119</h2>
+            </div >
+            <div class="contact">
+                <a href="https://www.linkedin.com/in/irina-bo%C5%BEani%C4%87//" target="_blank">
+                    <img src="img/contact/irina.png" alt="Irina">
+                </a>
+                <h2>Irina Božanić<br>2021/1050</h2>
+            </div>
+
+            <button type="button" onclick="closePopupInfo()">Nazad</button>
+
+        </div>
+        
+        
+    </div>
         `
     }
 }
@@ -37,13 +69,10 @@ class CustomFooter extends HTMLElement {
 
 customElements.define('custom-footer', CustomFooter);
 
-// RESPONZIVNOST
-function reponzivnostMob(){
-    document.getElementById("shop-grid-wrapper").style.gridTemplateColumns = "1fr";
+function openPopupInfo(){
+    document.getElementById("contact-info").classList.add("open-contact-info");
 }
-function responzivnostTablet(){
-    document.getElementById("shop-grid-wrapper").style.gridTemplateColumns = "1fr 1fr";
+function closePopupInfo(){
+    document.getElementById("contact-info").classList.remove("open-contact-info");
 }
-function responzivnostKomp(){
-    document.getElementById("shop-grid-wrapper").style.gridTemplateColumns = "1fr 1fr 1fr";
-}
+
