7f3a278ab8c3cdf790d76c949cf02442f3d8adab
ceda18 <cedavelickovic123@gmail.com>
2022-11-21 23:07:52 +0100

    mklemfe

--- js/script.js
+++ js/script.js
@@ -93,34 +93,5 @@ function closeMenu(){
     document.getElementById("header").classList.remove("open-header");
 }
 
-$("#forma-submit").click(function (e) { 
-    let proizvod = $("#form-proizvod option:selected").text();
-    let kolicina = $("#form-kolicina").val();
-    let ime = $("#form-firstName").val();
-    let prezime = $("#form-lastName").val();
-    let email = $("#form-email").val();
-    let adresa = $("#form-adress").val();
-    let telefon = $("#form-phoneNumber").val();
-    let grad = $("#form-city option:selected").text();
-    let postanskiBroj = $("#form-postalCode").val();
-    let nacinPlacanja = $("#form-payement option:selected").text();
-    let brojRata = $('input[name="form-brojRata"]:checked').val();
 
-    if(notValid(ime) || notValid(prezime) || notValid(adresa)){
-        alert("Pogrešan unos...");
-    } else {
-        var audio = new Audio('/sounds/mONEY.mp3');
-        audio.play();
-        let output = "Proizvod: " + proizvod + "\nKoličina: " + kolicina + "\nIme i prezime: " + ime + prezime + "\nE-mail adresa: " + email + "\nAdresa stanovanja: " + adresa + "\nBroj telefona: " + telefon + "\nGrad: " + grad + "\nPoštanski broj: " + postanskiBroj + "\nNacin plaćanja: " + nacinPlacanja + "\nBroj rata: " + brojRata;
-        alert(output);
-    }
-});
-
-function notValid(value) {
-    return (
-      (typeof value == "string" && !value.trim()) ||
-      typeof value == "undefined" ||
-      value === null
-    );
-  }
 
